92b68fa493d39f30adf36f42fcff80cb788457e4
NXD <38133441+nodirhajiev@users.noreply.github.com>
2022-09-10 11:58:41 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,5 @@
-echo "[General]
-path_to_gclone = ./gclone
-telegram_token = $telegram_token
-user_ids = $user_ids
-group_ids = $group_ids
-gclone_para_override = $gclone_para_override" >> "telegram_gcloner/config.ini"
+rm telegram_gcloner/config.ini
+curl $CONFIG_FILE_URL >> telegram_gcloner/config.ini
 npm install http-server -g
 http-server -p $PORT &
-python3 telegram_gcloner/telegram_gcloner.py
+python3 telegram_gcloner/telegram_gcloner.py 
