16ce2275f91bb58e1f8994c0d974d1e39fd309c2
NXD <38133441+nodirhajiev@users.noreply.github.com>
2022-10-03 20:28:56 +0500

    Update telegram_gcloner.py

--- telegram_gcloner/telegram_gcloner.py
+++ telegram_gcloner/telegram_gcloner.py
@@ -119,7 +119,7 @@ def main():
     load_handlers(updater.dispatcher)
 
     updater.start_polling()
-    updater.bot.send_message(chat_id=config.USER_IDS[0], text='Welcome to ☁️GClone Bot🤖.\nLet\'s copy some data!\n\n🧲Join to https://t.me/nodirdrivebots for mirroring and leeching!🧲')
+    updater.bot.send_message(chat_id=config.USER_IDS[0], text='Welcome to ☁️GClone Bot🤖.\nLet\'s copy some data!\n\n😛If you need more bookmarks, contact with nodir_khajiev.t.me!😛')
     updater.idle()
 
 
